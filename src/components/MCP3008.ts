6f5cfb549c18120f149c7d9539335b41ca307872
lifaon74 <valentinrich@gmail.com>
2016-11-12 17:41:50 +0100

    spi test

---
+++ src/components/MCP3008.ts
@@ -0,0 +1,54 @@
+import * as rpio from 'rpio';
+import * as SPI from 'spi';
+
+
+/**
+ * DOC: https://cdn-shop.adafruit.com/datasheets/MCP3008.pdf
+ **/
+
+export class MCP3008 {
+
+  static maxClockFrequency: number = 2e6; // 2Mhz
+
+  static getMaxSPIClockDivider(): number {
+    return Math.pow(2, Math.ceil(Math.log(250e6 / MCP3008.maxClockFrequency) / Math.log(2)));
+  }
+
+  static spi:any;
+  static initSPI() {
+    rpio.spiBegin();
+    rpio.spiSetClockDivider(MCP3008.getMaxSPIClockDivider());
+    // MCP3008.spi = new SPI.Spi('/dev/spidev0.0', {'mode': SPI.MODE['MODE_0']});
+    // MCP3008.spi.maxSpeed(MCP3008.maxClockFrequency); // in Hz
+    // MCP3008.spi.open();
+  }
+
+
+  constructor(public csPin: number) {
+    this.initCSPin();
+  }
+
+  initCSPin() {
+    rpio.open(this.csPin, rpio.OUTPUT, rpio.HIGH);
+  }
+
+  // 2828889 => rpio
+  // 3024565 => spi
+  read(chanel: number, callback: ((result: number) => any)) { //1461969us / 100000 = 14.61u
+    // data structure: [ sample, sample, null, b11, b10, b9, ..., b0]
+    rpio.write(this.csPin, rpio.LOW);
+
+    let txbuf = new Buffer([0x0, 0x0]);
+    let rxbuf = new Buffer(txbuf.length);
+
+    // MCP3008.spi.transfer(txbuf, rxbuf, (device, buf) => {
+    //   rpio.write(this.csPin, rpio.HIGH);
+    //   callback(1);
+    // });
+
+    rpio.spiTransfer(txbuf, rxbuf, txbuf.length);
+    rpio.write(this.csPin, rpio.HIGH);
+    callback(1);
+    return ((rxbuf[0] & 0b00011111) << 7) | (rxbuf[1] >> 1);
+  }
+}
--- src/gpio.ts
+++ src/gpio.ts
@@ -1,6 +1,7 @@
 import * as rpio from 'rpio';
 
-import { MCP3201 } from './components/MCP3201';
+// import { MCP3201 } from './components/MCP3201';
+import { MCP3008 } from './components/MCP3008';
 
 let NanoTimer:any = require('nanotimer');
 
@@ -8,12 +9,12 @@ let NanoTimer:any = require('nanotimer');
 rpio.init({gpiomem: false});
 
 
-MCP3201.initSPI();
+MCP3008.initSPI();
 // rpio.spiChipSelect(0);
 // rpio.spiSetCSPolarity(0, rpio.LOW);
 // (<any>rpio).spiSetDataMode(0);
 
-let mcp3201 = new MCP3201(5);
+let adc = new MCP3008(7);
 
 let status: number = rpio.HIGH;
 rpio.open(3, rpio.OUTPUT, status);
@@ -22,12 +23,19 @@ let timerObject = new NanoTimer();
 
 let loop = () => {
   let value: number;
-  let time = timerObject.time(() => {
-    value = mcp3201.read();
-  }, '', 'u');
-  console.log(time, value);
+  let t1 = process.hrtime();
+  adc.read(0, () => {
+    let t2 = process.hrtime(t1);
+    console.log(t2[0] / 1e9 + t2[1]);
+  });
+  // let time = timerObject.time(() => {
+  //   for(let i = 0; i < 100000; i++) {
+  //     value += adc.read();
+  //   }
+  // }, '', 'u');
+  // console.log(time, value);
   // (<any>rpio).msleep(100);
-  process.nextTick(loop);
+  // process.nextTick(loop);
 };
 
 
